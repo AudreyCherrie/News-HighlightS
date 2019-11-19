274edd161b59f6f9f09d6ea5a641c1ecf2739887
AudreyCherrie <mitchelaudrey@gmail.com>
2019-11-19 10:32:12 +0300

    update config.py file

---
+++ app/config.py
@@ -0,0 +1,35 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=bitcoin&from=2019-10-18&sortBy=publishedAt&apiKey ={}'
+    NEWS_SOURCES_URL ='https://newsapi.org/v2/top-headlines?country=au&apiKey={}'
+    NEWS_API_KEY =os.environ.get('NEWS_API_KEY')
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
